ab369d08dd79a0e3b63cfead74c129a4ba5f8c53
joshuous <joshuous@gmail.com>
2018-01-08 03:04:12 +0800

    kthread: Expose queuing_blocked() in kthread header
    This function is needed by the CPU-Boost driver.
    
    Signed-off-by: joshuous <joshuous@gmail.com>

--- kernel/kthread.c
+++ kernel/kthread.c
@@ -609,7 +609,7 @@ EXPORT_SYMBOL_GPL(kthread_worker_fn);
  * It happens when it is already pending in a worker list
  * or when it is being cancelled.
  */
-static inline bool queuing_blocked(struct kthread_worker *worker,
+inline bool queuing_blocked(struct kthread_worker *worker,
 				   struct kthread_work *work)
 {
 	lockdep_assert_held(&worker->lock);
